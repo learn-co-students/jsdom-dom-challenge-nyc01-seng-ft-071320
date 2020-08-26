495736d00ca594554cc1e927db90a1f9a2c1d12b
redazures <jwu_nyc@yahoo.com>
2020-08-26 16:50:45 -0400

    4 deliverables

--- js/challenge.js
+++ js/challenge.js
@@ -4,20 +4,31 @@ document.addEventListener("DOMContentLoaded", () => {
     const counter = document.querySelector('#counter')
     //counter is the main thing that we are manipulating
 
-    function counterUp () {
+    function counterUp2 () {
         setInterval(function(){
           counter.textContent++;
         }, 1000);
     }
 
+    function counterUp() {
+        setInterval(function(){
+            if (document.querySelector('#pause').innerText=="resume") counter.textContent;
+            else {counter.textContent++};
+        }, 1000);
+    } 
+
     function clickHandler() {
+        let toggle = document.querySelector('#pause').innerText
         document.addEventListener('click', function(e){
-            // console.log(e.target)
-            if(e.target.matches("#plus")){
+            console.log(e.target)
+            if(e.target.matches("#plus") && toggle=="resume"){
             counter.textContent++;
-            } else if(e.target.matches('#minus')){
+            } else if(e.target.matches('#minus' && toggle=="resume")){
             counter.textContent = counter.textContent - 1;
-            } else if(e.target.matches('#heart')){
+            } else if(e.target.matches('#pause')){
+            if (toggle == "pause"){document.querySelector('#pause').innerText="resume"}
+            else if (toggle == "resume"){document.querySelector('#pause').innerText="pause"}
+            } else if(e.target.matches('#heart') && toggle=="resume"){
                 let topList = document.querySelector(".likes")
                 if (!topList.hasChildNodes()) {
                     listElement=document.createElement("li")
@@ -37,8 +48,8 @@ document.addEventListener("DOMContentLoaded", () => {
                     }
                 }
             }
-            else if(e.target.matches('#pause')){
-            console.log(e.target)
+            else {
+                
           }
         }) 
       }
