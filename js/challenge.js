b84d36d9a6bbb0024d73ab54619d80568417bf1f
Mimioj101 <mimioj@gmail.com>
2020-08-26 13:44:01 -0400

    setup

---
+++ js/challenge.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(e){
+
+
+    function secondAdder() {
+        let counter = document.querySelector("#counter").innerText
+        let countNum = parseInt(counter, 10)
+        let newNum = counter += 1
+        counter = newNum.toString()
+        console.log(counter)
+    }
+
+//    window.setInterval(secondAdder, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+secondAdder()
+
+})
