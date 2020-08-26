b865b7af48cad8d8eb3f4bc14133d44f25ac8976
junko911 <junkotahara911@gmail.com>
2020-08-26 13:45:38 -0400

    First commit

--- js/challenge.js
+++ js/challenge.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 
-    function secondAdder() {
-        let counter = document.querySelector("#counter").innerText
-        let countNum = parseInt(counter, 10)
-        let newNum = counter += 1
-        counter = newNum.toString()
-        console.log(counter)
-    }
+    // function secondAdder() {
+    //     let counter = document.querySelector("#counter").innerText
+    //     let countNum = parseInt(counter, 10)
+    //     let newNum = counter += 1
+    //     counter = newNum.toString()
+    //     console.log(counter)
+    // }
 
 //    window.setInterval(secondAdder, 1000)
 
@@ -24,6 +24,6 @@ document.addEventListener("DOMContentLoaded", function(e){
 
 
 
-secondAdder()
+// secondAdder()
 
 })
